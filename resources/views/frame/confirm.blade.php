5590d8165e75e181dde249d28560e4a18921c2bd
khanhpv4 <khanhpoly@gmail.com>
2018-10-18 22:57:47 +0700

    fix frame

--- resources/views/frame/confirm.blade.php
+++ resources/views/frame/confirm.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Lịch sử nạp thẻ</title>
     <!-- Bootstrap core CSS-->
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -19,33 +19,31 @@
     <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
-<body>
-        <div class="row">
-                <div class="col-md-6 col-md-offset-3" style="margin:0 auto;padding:0;width:100%;text-align:center" >
-                    <h4 class="text-center">Thông tin nạp thẻ</h4>
-                    <p class="text-success">{{ $mess }}</p>
-                    <table class="table table-condensed table-striped">
-                        <thead>
-                            <tr>
-                                <th>Loại thẻ</th>
-                                <th>Mệnh giá</th>
-                                <th>Frame ID</th>
-                                <th>Số tiền trong frame</th>
-                                <th>Người nhận</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                                <tr>
-                                    <td>{{ $card_name }}</td>
-                                    <td>{{ number_format($result->price)}} đ</td>
-                                    <td>{{ $result->link_id }}</td>
-                                    <td>{{ number_format($price_of_link)}} đ</td>
-                                    <td>{{ $username }}</td>
-                               </tr>
-                        </tbody>
-                    </table>
-                    button type="button" onclick="goBack()" class="btn btn-sm btn-info">Quay lại</a>
-                </div>
-        </div>
+<body style="background:#efeded">
+       <div class="container" style="width:600px; height: 600px; border:1px solid #eee;background:#fff;padding-left:5%;padding-right: 5%">
+            <div class="row">
+                    <div class="col-md-12 col-xs-12" style="margin:0 auto;padding:0;width:100%;text-align:center" >
+                        <br>
+                        <h4 class="text-center">Thông tin nạp thẻ</h4>
+                        <p class="text-success">{{ $mess }}</p>
+                        <p class="text-danger">Nhập vào số điện thoại, bạn vừa nhập ở phần nạp tiền, để kiểm tra trạng thái thẻ nạp</p>
+                        <form action="">
+                            <input type="search"  placeholder="Nhập vào số điện thoại">
+                            <button class="btn-primary">Search</button>
+                        </form>
+                    </div>
+            </div>
+            <br>
+            <div class="row">
+                <table class="table table-sm">
+                    <thead>
+                        <th>Id</th>
+                        <th>Số điện thoại</th>
+                        <th>Mệnh giá</th>
+                        <th>Trạng thái</th>
+                    </thead>
+                </table>
+            </div>
+       </div>
 </body>
 </html>
--- resources/views/frame/napthe.blade.php
+++ resources/views/frame/napthe.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Nạp thẻ</title>
     <!-- Bootstrap core CSS-->
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -20,83 +20,68 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="row">
-        <div class="col-md-12 col-sm-12">
-            @if(session()->has('message'))
-              <div class="alert alert-success">
-                  {{ session()->get('message') }}
-              </div>
-          @endif
-            @if(session()->has('error'))
-              <div class="alert alert-danger">
-                  {{ session()->get('error') }}
-              </div>
-          @endif
-              <br>
-            <div class="panel panel-default panel-table">
-                <div class="panel-heading">
-                    <h4>Nạp thẻ không cần đăng nhập</h4>
-                    <p class="text-danger">* Nếu bạn quên id frame, vui lòng đăng nhập và vào phần <strong>Tích hợp vào website</strong> để lấy id frame</p>
-                    <div class="tools"><span class="icon mdi mdi-download"></span><span class="icon mdi mdi-more-vert"></span></div>
+    <div class="container" style="padding-left:10%;padding-right: 10%">
+            <div class="row">
+                    <div class="col-12 col-xs-12">
+                        @if(session()->has('message'))
+                          <div class="alert alert-success">
+                              {{ session()->get('message') }}
+                          </div>
+                      @endif
+                        @if(session()->has('error'))
+                          <div class="alert alert-danger">
+                              {{ session()->get('error') }}
+                          </div>
+                      @endif
+                                <h4>Nạp thẻ không cần đăng nhập</h4>
+                                <p class="text-danger">* Nếu bạn quên id frame, vui lòng đăng nhập và vào phần <strong>Tích hợp vào website</strong> để lấy id frame</p>
+                             
+                                <form action="{{ route('frame.createPayment') }}" method="POST" >
+                                  @csrf
+                                    <input type="hidden" name="nap_the" value="3">
+                                    <div class="form-group row">
+                                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Frame ID</label>
+                                            <input type="number" name="link_id" placeholder="Nhập vào frame id" class="form-control" value="{{ $result->id }}" readonly>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Loại thẻ</label>
+                                        <select class="form-control" name="card_type" id="card_type" class="" required>
+                                                @foreach ($card as $value)
+                                                    <option value="{{ $value->card_code }}">{{ $value->card_name }}</option>   
+                                                @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Số điện thoại</label>
+                                        <input type="number" class="form-control" value="" placeholder="Nhập vào sđt của bạn" name="phone" required>
+                                    </div>
+                                    <div class="form-group row">
+                                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Mệnh giá</label>
+            
+                                            <select required class="form-control" name="card_price">
+                                              <option value="10000">10.000&nbsp;₫</option>
+                                              <option value="20000">20.000&nbsp;₫</option>
+                                              <option value="30000">30.000&nbsp;₫</option>
+                                              <option value="50000">50.000&nbsp;₫</option>
+                                              <option value="100000">100.000&nbsp;₫</option>
+                                              <option value="200000">200.000&nbsp;₫</option>
+                                              <option value="300000">300.000&nbsp;₫</option>
+                                              <option value="500000">500.000&nbsp;₫</option>
+                                              <option value="1000000">1.000.000&nbsp;₫</option>
+                                           </select>
+                                    </div>
+                                    <div class="form-group row">
+                                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Mã Pin</label>
+                                            <input type="number" name="card_pin" placeholder="Nhập vào mã pin" class="form-control" required>
+                                    </div>
+                                    <div class="form-group row">
+                                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Mã Serial</label>
+                                            <input type="number" name="card_seria" placeholder="Nhập vào số serial" class="form-control" required>
+                                    </div>
+                                    <button class="btn btn-primary">Nạp</button>
+                              </form>
+                              <br>
+                  </div>
                 </div>
-            <div class="table-responsive">
-             
-            <table class="table table-condensed table-hover table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>Frame</th>
-                        <th>Loại Thẻ</th>
-                        <th>Mệnh giá</th>
-                        <th>Pin</th>
-                        <th>Serial</th>
-                        <th>Hành động</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <form action="{{ route('frame.createPayment') }}" method="POST" >
-                      @csrf
-                    <input type="hidden" name="nap_the" value="3">
-                      <tr>
-                          <td class="number" style="width: 20%;">
-                             <input type="number" name="link_id" placeholder="Nhập vào frame id" class="form-control" value="{{ $result->id }}" readonly>
-                          </td>
-                          <td class="number" style="width: 20%;">
-                              <select class="form-control" name="card_type" id="card_type" class="" required>
-                                @foreach ($card as $value)
-                                  <option value="{{ $value->card_code }}">{{ $value->card_name }}</option>   
-                                @endforeach
-                                </select>
-                          </td>
-                          <td class="number" style="width: 20%;">
-                              <select required class="form-control" name="card_price">
-                                  <option value="10000">10.000&nbsp;₫</option>
-                                  <option value="20000">20.000&nbsp;₫</option>
-                                  <option value="30000">30.000&nbsp;₫</option>
-                                  <option value="50000">50.000&nbsp;₫</option>
-                                  <option value="100000">100.000&nbsp;₫</option>
-                                  <option value="200000">200.000&nbsp;₫</option>
-                                  <option value="300000">300.000&nbsp;₫</option>
-                                  <option value="500000">500.000&nbsp;₫</option>
-                                  <option value="1000000">1.000.000&nbsp;₫</option>
-                               </select>
-                          </td>
-                          <td>
-                            <input type="number" name="card_pin" placeholder="Nhập vào mã pin" class="form-control" required>
-                          </td>
-                          <td>
-                            <input type="number" name="card_seria" placeholder="Nhập vào số serial" class="form-control" required>
-                          </td>
-                          <td class="number" style="width: 5%;">
-                              <button class="btn btn-primary">Nạp</button>
-                          </td>
-                      </tr>
-                  </form>
-                </tbody>
-            </table>
-            </div>
-        </div>
-      </div>
-    </div>
-
 </body>
 </html>
